a07867dd11036f836444834766f7aff4ed71a66d
ngocthetran <tranthengoc02072002@gmail.com>
2022-08-18 13:29:38 +0700

    update

---
+++ app/src/main/java/vidu/demo/myapplication/Adapter/AdapterChuNangAdd.java
@@ -0,0 +1,2 @@
+package vidu.demo.myapplication.Adapter;public class AdapterChuNangAdd {
+}
---
+++ app/src/main/java/vidu/demo/myapplication/FragmnetChucNang/CalendarFragment_add.java
@@ -0,0 +1,66 @@
+package vidu.demo.myapplication.FragmnetChucNang;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import vidu.demo.myapplication.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link CalendarFragment_add#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class CalendarFragment_add extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public CalendarFragment_add() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment CalendarFragment_add.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static CalendarFragment_add newInstance(String param1, String param2) {
+        CalendarFragment_add fragment = new CalendarFragment_add();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_calendar_add, container, false);
+    }
+}
